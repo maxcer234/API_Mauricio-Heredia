ff5af901ec2737c060ac3bf6f9d91b8bf8f383a4
Maurcio Heredia <maurih46322945@gmail.com>
2024-06-03 15:03:22 -0300

    se termino el JAVASCRIPT

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const characterListElement = document.getElementById('character-list');
 
-    function fetchRickAndMortyCharacters() {
-        fetch('https://rickandmortyapi.com/api/character')
-            .then(response => response.json())
-            .then(data => {
-                const characters = data.results;
-                characters.forEach(character => {
-                    const characterItem = document.createElement('div');
-                    characterItem.className = 'character-item clearfix';
-                    characterItem.innerHTML = `
-                        <img src="${character.image}" alt="${character.name}" width="50">
-                        <h3>${character.name}</h3>
-                        <p>Location: ${character.location.name}</p>
-                    `;
-                    characterListElement.appendChild(characterItem);
-                });
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }
+    const fetchRickAndMortyCharacters = async () => {
+        try {
+            const response = await fetch('https://rickandmortyapi.com/api/character');
+            const data = await response.json();
+            const characters = data.results;
+
+            characters.forEach(character => {
+                const characterItem = document.createElement('div');
+                characterItem.className = 'character-item';
+                characterItem.innerHTML += `
+                    <img src="${character.image}" alt="${character.name}">
+                    <h3>${character.name}</h3>
+                    <p>Location: ${character.location.name}</p>
+                `;
+                characterListElement.appendChild(characterItem);
+            });
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
     fetchRickAndMortyCharacters();
 });
