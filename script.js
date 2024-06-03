2be191f1fd3ebdf1967b857cf7a3f89d0a0df05e
Maurcio Heredia <maurih46322945@gmail.com>
2024-06-03 15:01:42 -0300

    Se cambio el HTML Y JAVASCRIPT

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const URL = "https://api.waifu.im/search"
+document.addEventListener('DOMContentLoaded', function() {
+    const characterListElement = document.getElementById('character-list');
 
+    function fetchRickAndMortyCharacters() {
+        fetch('https://rickandmortyapi.com/api/character')
+            .then(response => response.json())
+            .then(data => {
+                const characters = data.results;
+                characters.forEach(character => {
+                    const characterItem = document.createElement('div');
+                    characterItem.className = 'character-item clearfix';
+                    characterItem.innerHTML = `
+                        <img src="${character.image}" alt="${character.name}" width="50">
+                        <h3>${character.name}</h3>
+                        <p>Location: ${character.location.name}</p>
+                    `;
+                    characterListElement.appendChild(characterItem);
+                });
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }
+
+    fetchRickAndMortyCharacters();
+});
